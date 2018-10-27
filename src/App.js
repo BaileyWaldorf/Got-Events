46c778045f54d9b05056838e100d4491b9ace0c4
Brendon Lyra <brendonlyra@knights.ucf.edu>
2018-10-26 21:43:56 -0400

    added login page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Router from './Router';
-
+import Login from './components/Login'
 class App extends Component {
+
+    // render() {
+    //     return (
+    //         <div className="App">
+    //             <Header/>
+    //             <Router />
+    //         </div>
+    //     );
+    // }
 
     render() {
         return (
-            <div className="App">
-                <Header/>
-                <Router />
-            </div>
+            <Login></Login>
         );
     }
 }
---
+++ src/components/Login.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+export default class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            buttonEnabled: true,
+            authenticated: 'false'
+        }
+    }
+
+    handleChangeLogin = (event) => {
+        this.setState({username: event.target.value});
+    }
+
+    handleChangePassword = (event) => {
+        this.setState({password: event.target.value});
+    }
+
+    handleSubmit = (event) => {
+        let temp_usrname = this.state.username;
+        let temp_passwrd = this.state.password;
+        
+        this.setState({buttonEnabled: false});
+
+    }
+
+    render() {
+
+        const registerLink = <a href={"RegisterPage"}>Register here</a>
+
+        return (
+            <div className="LoginPage">
+                <div className="LoginContainer">
+                    <h1 style={{color:"white", marginTop:'5%', paddingBottom:'35%'}}>Got-Events</h1>
+                    <div>
+                        <input onChange={this.handleChangeLogin} className="LoginInput" placeholder="Login" type='text'></input>
+                    </div>
+                    <div>
+                        <input onChange={this.handleChangePassword} className="LoginInput" placeholder="Password" type='password'></input>
+                    </div>
+                    <div>
+                        <button onSubmit={this.handleSubmit} className="LoginButton">Log In</button>
+                    </div>
+                    <div style={{ color: 'white', paddingTop:'35%' }}>Don't have an account yet? {registerLink}</div>
+                </div>
+            </div>
+        );
+    }
+}
