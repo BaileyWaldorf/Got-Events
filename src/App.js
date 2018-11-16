96d69126d927c96b12edf5a9b6ade768c0e78f99
Brendon Lyra <brendonlyra@knights.ucf.edu>
2018-11-16 16:07:05 -0500

    Updated login, register pages

--- src/App.js
+++ src/App.js
@@ -2,28 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Router from './Router';
-import Login from './components/Login'
+import Login from './components/Login'                  //gud
 import CreateEvents from './components/CreateEvents'
-import Register from './components/Register'
+import Register from './components/Register'            
 
 
 class App extends Component {
 
     render() {
+        // return (
+        //     <div className="App">
+        //         <Header/>
+        //         <Router />
+        //         <div eventname="name" style={{marginLeft:"50%"}}>Event Name: Spirit Splash</div>
+        //         <div style={{marginLeft:"50%"}}>Location: Reflection Pond </div>
+        //         <div style={{marginLeft:"50%"}}>Date: November 9th 2018</div>
+        //         <div style={{marginLeft:"50%"}}>Time: 1:00 pm</div>
+        //         <div style={{marginLeft:"50%"}}>Rating: 5.0/5.0</div>
+        //         <div style={{marginLeft:"50%"}}>Description: Come to the Reflection Pond for the famous Spirit Splash!</div>
+        //         <div style={{marginLeft:"50%"}}>There is music, splashing, water, a fountain, and best of all, RUBBER DUCKIES!</div>
+                
+                
+        //     </div>
+        // );
         return (
-            <div className="App">
-                <Header/>
-                <Router />
-                <div eventname="name" style={{marginLeft:"50%"}}>Event Name: Spirit Splash</div>
-                <div style={{marginLeft:"50%"}}>Location: Reflection Pond </div>
-                <div style={{marginLeft:"50%"}}>Date: November 9th 2018</div>
-                <div style={{marginLeft:"50%"}}>Time: 1:00 pm</div>
-                <div style={{marginLeft:"50%"}}>Rating: 5.0/5.0</div>
-                <div style={{marginLeft:"50%"}}>Description: Come to the Reflection Pond for the famous Spirit Splash!</div>
-                <div style={{marginLeft:"50%"}}>There is music, splashing, water, a fountain, and best of all, RUBBER DUCKIES!</div>
-                
-                
-            </div>
+            <CreateEvents></CreateEvents>
         );
     }
 
--- src/components/Register.js
+++ src/components/Register.js
@@ -7,27 +7,66 @@ export default class Login extends Component {
         this.state = {
             username: '',
             password: '',
+            rePassword: '',
+            school: '',
+            location: '',
+            description: '',
             buttonEnabled: true,
-            authenticated: 'false'
+            IsSuperAdmin: 'hidden',
+            schoolPlaceholder: 'school',
+            buttonColor1: 'lightslategray',
+            buttonColor2: 'lightslategray',
+            buttonColor3: 'lightslategray',
+            failedlLogin: 'hidden',
+            authenticated: ''
+
         }
     }
 
     handleChangeLogin = (event) => {
-        this.setState({username: event.target.value});
+        this.setState({ username: event.target.value });
     }
 
     handleChangePassword = (event) => {
-        this.setState({password: event.target.value});
+        this.setState({ password: event.target.value });
+    }
+
+    handleChangerePassword = (event) => {
+        this.setState({ rePassword: event.target.value });
+    }
+
+    handleChangeSchool = (event) => {
+        this.setState({ school: event.target.value });
+    }
+
+    handleChangeLocation = (event) => {
+        this.setState({ location: event.target.value });
+    }
+
+    handleChangeDescription = (event) => {
+        this.setState({ description: event.target.value });
     }
 
     handleSubmit = (event) => {
         //let temp_usrname = this.state.username;
         //let temp_passwrd = this.state.password;
-        
-        this.setState({buttonEnabled: false});
+
+        this.setState({ buttonEnabled: false });
 
         console.log("submit")
 
+    }
+
+    button1Click = (event) => {
+        this.setState({ buttonColor1: 'black', buttonColor2: 'lightslategray', buttonColor3: 'lightslategray', schoolPlaceholder: 'school', IsSuperAdmin: 'hidden' })
+    }
+
+    button2Click = (event) => {
+        this.setState({ buttonColor1: 'lightslategray', buttonColor2: 'black', buttonColor3: 'lightslategray', schoolPlaceholder: 'school', IsSuperAdmin: 'hidden' })
+    }
+
+    button3Click = (event) => {
+        this.setState({ buttonColor1: 'lightslategray', buttonColor2: 'lightslategray', buttonColor3: 'black', schoolPlaceholder: 'Create school', IsSuperAdmin: 'visible' })
     }
 
     render() {
@@ -36,32 +75,41 @@ export default class Login extends Component {
 
         return (
             <div>
-            <div>
-                <Header></Header>
-            <div className="LoginPage">
-                <div className="LoginContainer">
-                    <h1 style={{color:"white", marginTop:'5%', paddingBottom:'15%'}}>Register</h1>
-                    <div>
-                        <input onChange={this.handleChangeLogin} className="LoginInput" placeholder="Username" type='text'></input>
-                    </div>
-                    <div>
-                        <input onChange={this.handleChangePassword} className="LoginInput" placeholder="Password" type='password'></input>
-                    </div>
-                    <div>
-                        <input onChange={this.handleChangePassword} className="LoginInput" placeholder="Confirm Password" type='password'></input>
-                    </div>
-                    <div>
-                        <input onChange={this.handleChangePassword} className="LoginInput" placeholder="School"></input>
-                    </div>
-                    <div>
-                        <input onChange={this.handleChangePassword} className="LoginInput" placeholder="User Type "></input>
-                    </div>
-                    <div>
-                        <button onClick={this.handleSubmit}  className="LoginButton" style={{marginTop:"35%"}}>Register</button>
+                <div>
+                    <Header></Header>
+                    <div className="LoginPage">
+                        <div className="LoginContainer">
+                            <h1 style={{ color: "white", marginTop: '5%', paddingBottom: '15%' }}>Register</h1>
+                            <div>
+                                <input onChange={this.handleChangeLogin} className="LoginInput" placeholder="Username" type='text'></input>
+                            </div>
+                            <div>
+                                <input onChange={this.handleChangePassword} className="LoginInput" placeholder="Password" type='password'></input>
+                            </div>
+                            <div>
+                                <input onChange={this.handleChangerePassword} className="LoginInput" placeholder="Confirm Password" type='password'></input>
+                            </div>
+                            <div style={{ marginTop: '4%' }}>
+                                <button onClick={this.button1Click} style={{ backgroundColor: this.state.buttonColor1, borderWidth: '1px', width: '80px', height: '30px', outlineWidth: '0', alignContent: 'center' }}>Student</button>
+                                <button onClick={this.button2Click} style={{ backgroundColor: this.state.buttonColor2, borderWidth: '1px', width: '80px', height: '30px', outlineWidth: '0', alignContent: 'center' }}>Admin</button>
+                                <button onClick={this.button3Click} style={{ backgroundColor: this.state.buttonColor3, borderWidth: '1px', width: '100px', height: '30px', outlineWidth: '0', alignContent: 'center' }}>SuperAdmin</button>
+                            </div>
+                            <div>
+                                <input onChange={this.handleChangeSchool} className="LoginInput" placeholder={this.state.schoolPlaceholder}></input> {/*School(all of them), super: school location, description */}
+                            </div>
+                            <div style={{ visibility: this.state.IsSuperAdmin }}>
+                                <input onChange={this.handleChangeLocation} className="LoginInput" placeholder="Location"></input>
+                            </div>
+                            <div style={{ visibility: this.state.IsSuperAdmin }}>
+                                <input onChange={this.handleChangeDescription} className="LoginInput" placeholder="Description"></input>
+                            </div>
+                            <div style={{ marginTop: '8%', visibility: this.state.failedlLogin }}>FAILED</div>
+                            <div>
+                                <button onClick={this.handleSubmit} className="LoginButton" style={{ marginTop: "0%" }}>Register</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            </div>
             </div>
         );
     }
