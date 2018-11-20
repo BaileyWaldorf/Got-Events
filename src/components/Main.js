558e5dca6635fad9cb3d43c03a5588ec75699640
Joshua Sylvestre <joshuasylvestre@knights.ucf.edu>
2018-11-20 00:32:22 -0500

    Introduces routing main

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {Switch, Route} from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';
+import CreateEvents from './CreateEvents';
+import MyEventsContainer from '../containers/MyEventsContainer';
+
+const Main = () => (
+        <Switch>
+            <Route exact path='/' component={Login}></Route>
+            <Route path='/register' component={Register}></Route>
+            <Route path='/createEvents' component={CreateEvents}></Route>
+            <Route path='/myEvents' component={MyEventsContainer}></Route>
+        </Switch>
+)
+
+export default Main;
